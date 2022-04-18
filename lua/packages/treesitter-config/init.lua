3f421d43f3ca152c72ae4f6da128409f6a8359a2
Vallahor <vallahor91@gmail.com>
2022-04-18 16:58:19 -0300

    treesitter: just what I need

--- lua/packages/treesitter-config/init.lua
+++ lua/packages/treesitter-config/init.lua
@@ -1,5 +1,5 @@
 require 'nvim-treesitter.configs'.setup {
-  ensure_installed = "all",
+  ensure_installed = { "lua", "c", "cpp", "javascript", "typescript", "tsx", "zig", "jsonc", "json", "css", "scss" },
   highlight = {
     enable = true,
   },
