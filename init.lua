a10ef6c1fcddb204eaf0e4d0948e3457c3050cce
vallahor <vallahor91@gmail.com>
2022-09-03 02:02:36 -0300

    changes

--- init.lua
+++ init.lua
@@ -16,10 +16,20 @@ require("packer").startup(function(use)
 
     -- @languages
     use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+    use({ "vim-kind/vim-kind" })
     -- use({ "ziglang/zig.vim" })
 
     use({ "ggandor/lightspeed.nvim" })
     use({ "kdheepak/lazygit.nvim" })
+
+    use({ "kyazdani42/nvim-tree.lua" })
+    use({ 'numToStr/Comment.nvim' })
+
+    use({ "ojroques/nvim-bufdel" })
+
+    use({ "tpope/vim-surround" })
+	use({ "tpope/vim-repeat" })
+
 
     if packer_bootstrap then
         require("packer").sync()
@@ -56,7 +66,7 @@ config_global.lazyredraw = true
 config_global.smartcase = true
 config_global.inccommand = "nosplit"
 config_global.backspace = "indent,eol,start"
-config_global.timeoutlen = 0
+-- config_global.timeoutlen = 0
 config_global.shortmess = "aoOtTIc"
 config_global.mouse = "a"
 config_global.mousefocus = true
@@ -79,48 +89,50 @@ config_buffer.swapfile = false
 
 
 local ok, nvim_treesitter = pcall(require, "nvim-treesitter.configs")
-nvim_treesitter.setup({
-    ensure_installed = {
-        "lua",
-        "bash",
-        "c",
-        "cpp",
-        "javascript",
-        "typescript",
-        "zig",
-        "jsonc",
-        "query",
-        "python",
-        "rust",
-        "go",
-    },
-    highlight = {
-        enable = true,
-    },
-    indent = {
-        enable = false,
-    },
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            -- init_selection = "m",
-            node_incremental = "v",
-            node_decremental = "V",
-            scope_incremental = "<c-/>",
+if ok then
+    nvim_treesitter.setup({
+        ensure_installed = {
+            "lua",
+            "bash",
+            "c",
+            "cpp",
+            "javascript",
+            "typescript",
+            "zig",
+            "jsonc",
+            "query",
+            "python",
+            "rust",
+            "go",
         },
-    },
-})
+        highlight = {
+            enable = true,
+        },
+        indent = {
+            enable = false,
+        },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "m",
+                node_incremental = "m",
+                node_decremental = "M",
+                scope_incremental = "<c-/>",
+            },
+        },
+    })
+end
 
 require("nvim-treesitter.highlight").set_custom_captures({
-    ["js.named_import"] = "TSLiteral",
-    ["js.import"] = "TSLiteral",
-    ["js.keyword"] = "TSOperator",
-    ["js.keyword_bold"] = "TSInclude",
-    ["js.arrow_func"] = "TSKeyword",
-    ["js.opening_element"] = "TSElement",
-    ["js.closing_element"] = "TSElement",
-    ["js.self_closing_element"] = "TSElement",
-    ["zig.assignop"] = "TSOperator",
+   ["js.named_import"] = "TSLiteral",
+   ["js.import"] = "TSLiteral",
+   ["js.keyword"] = "TSOperator",
+   ["js.keyword_bold"] = "TSInclude",
+   ["js.arrow_func"] = "TSKeyword",
+   ["js.opening_element"] = "TSElement",
+   ["js.closing_element"] = "TSElement",
+   ["js.self_closing_element"] = "TSElement",
+   ["zig.assignop"] = "TSOperator",
 })
 
 vim.cmd [[colorscheme gruvball]]
@@ -136,6 +148,49 @@ lightspeed.setup({
     repeat_ft_with_target_char = true,
 })
 
+local ok, nvim_tree = pcall(require, "nvim-tree")
+if  ok then
+    nvim_tree.setup({
+    renderer = {
+        indent_markers = {
+            enable = false,
+            icons = {
+                corner = "└ ",
+                edge = "│ ",
+                none = "  ",
+            },
+        },
+        icons = {
+            webdev_colors = false,
+            show = {
+                folder = false,
+                file = false,
+                folder_arrow = true,
+            },
+        },
+    },
+    git = {
+        enable = false,
+        ignore = false,
+        timeout = 400,
+    },
+})
+end
+local ok, nvim_comment = pcall(require, "Comment")
+if ok then
+    nvim_comment.setup()
+    local ft = require('Comment.ft')
+    ft.set('kind', '//%s')
+end
+
+local ok, bufdel = pcall(require, "bufdel")
+if ok then
+    bufdel.setup({
+        next = "cycle", -- or 'alternate'
+        quit = false,
+    })
+end
+
 -- MAPPING --
 
 local map = vim.keymap.set
@@ -144,13 +199,17 @@ map("n", "<space>", "<nop>")
 
 vim.g.mapleader = " "
 
-map("n", "<c-w>", "<cmd>bd #<cr>")
+map("n", "<c-w>", "<cmd>BufDel<CR>")
 
 map("n", "<esc>", "<cmd>nohl<cr><esc>")
 
 map("n", "<c-g>", "<cmd>LazyGit<cr>")
+map("n", "<c-s>", "<cmd>NvimTreeToggle<cr>")
+map("n", "<c-;>", "<cmd>NvimTreeFocus<cr>")
 
 map("c", "<c-v>", '<c-r>"')
+
+map("v", "v", 'V')
 
 map({ "i", "c" }, "<c-bs>", "<c-w>")
 
@@ -172,24 +231,31 @@ map({ "n", "v" }, "k", "gk")
 map({ "n", "v" }, "<c-enter>", "<cmd>w!<CR>")
 map({ "n", "v" }, "<s-enter>", "<cmd>w!<CR>")
 
+map("n", "<f4>", "<cmd>:e ~/.config/nvim/init.lua<CR>")
 map("n", "<f5>", "<cmd>so %<CR>")
 
 map("n", "<c-f>", ":e ")
 map("n", "<c-space>", ":b ")
+
+-- map("n", "<c-c>", "<cmd>!kind %:p<cr>")
+map("n", "<c-c>", "<cmd>!kind2 run %:p<cr>")
 
 map({ "n", "v" }, "<c-h>", "<c-w>h")
 map({ "n", "v" }, "<c-j>", "<c-w>j")
 map({ "n", "v" }, "<c-k>", "<c-w>k")
 map({ "n", "v" }, "<c-l>", "<c-w>l")
 
-map({ "n", "v" }, "[", "{")
-map({ "n", "v" }, "]", "}")
+map({ "n", "v" }, "<c-p>", "{")
+map({ "n", "v" }, "<c-n>", "}")
+
+-- map({ "n", "v" }, "[", "{")
+-- map({ "n", "v" }, "]", "}")
 
 -- map({ "n", "v" }, "<c-n>", "}")
 -- map({ "n", "v" }, "<c-p>", "{")
 --
-map({ "n", "v" }, "{", "<c-u>")
-map({ "n", "v" }, "}", "<c-d>")
+-- map({ "n", "v" }, "{", "<c-u>")
+-- map({ "n", "v" }, "}", "<c-d>")
 
 -- map("n", "H", "<c-u>zz")
 -- map("n", "L", "<c-d>zz")
@@ -203,9 +269,10 @@ map("n", "<c-]>", "<cmd>clo<cr>")
 map("n", "<c-0>", "<c-w>o")
 map("n", "<c-9>", "<c-w>r")
 
+map("v", "z", "<Plug>Lightspeed_s")
+map("v", "Z", "<Plug>Lightspeed_S")
 
-map("v", "s", "<Plug>Lightspeed_s")
-map("v", "S", "<Plug>Lightspeed_S")
+map("v", "s", "<Plug>VSurround")
 
 vim.cmd([[
 vnoremap J :m '>+1<CR>gv=gv
@@ -221,11 +288,11 @@ augroup end
 map <expr> ; g:lightspeed_last_motion == 'sx' ? "<Plug>Lightspeed_;_sx" : "<Plug>Lightspeed_;_ft"
 map <expr> , g:lightspeed_last_motion == 'sx' ? "<Plug>Lightspeed_,_sx" : "<Plug>Lightspeed_,_ft"
 
-" Highlight extra whitespace.
-" http://vim.wikia.com/wiki/Highlight_unwanted_spaces
-highlight ExtraWhitespace ctermbg=lightred guibg=lightred
-" Show trailing whitespace and spaces before a tab:
-match ExtraWhitespace /\s\+$\| \+\ze\t/
+"" Highlight extra whitespace.
+"" http://vim.wikia.com/wiki/Highlight_unwanted_spaces
+"highlight ExtraWhitespace ctermbg=lightred guibg=lightred
+"" Show trailing whitespace and spaces before a tab:
+"match ExtraWhitespace /\s\+$\| \+\ze\t/
 
 " au BufEnter *.scm set filetype=query
 
@@ -234,4 +301,21 @@ autocmd FileType javascript setlocal shiftwidth=2 softtabstop=2 expandtab
 autocmd FileType typescript setlocal shiftwidth=2 softtabstop=2 expandtab
 autocmd FileType json setlocal shiftwidth=2 softtabstop=2 expandtab
 
+autocmd FileType kind setlocal shiftwidth=2 softtabstop=2 expandtab
+autocmd FileType kind setlocal syntax=on
+
+autocmd BufEnter *.kind2 set ft=kind
+
+ autocmd BufEnter *.kind2 setlocal shiftwidth=2 softtabstop=2 expandtab
+"autocmd FileType kind2 setlocal shiftwidth=2 softtabstop=2 expandtab
+" autocmd FileType kind2 setlocal syntax=on
+
+fun! TrimWhitespace()
+	let l:save = winsaveview()
+	keeppatterns %s/\s\+$//e
+	call winrestview(l:save)
+endfun
+autocmd BufWritePre * :call TrimWhitespace()
+
 ]])
+
