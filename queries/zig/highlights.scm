7fcbf3ca6182b6c76e66281ff1fee35d244e1818
Vallahor <vallahor91@gmail.com>
2023-09-10 16:12:40 -0300

    zig treesitter improvements

--- queries/zig/highlights.scm
+++ queries/zig/highlights.scm
@@ -9,15 +9,42 @@
  [
   variable_type_function: (IDENTIFIER)
   field_access: (IDENTIFIER)
+  function: (IDENTIFIER)
  ] @type
  (#lua-match? @type "^[A-Z][a-z_0-9]*")
 )
+
+(
+ [
+  variable_type_function: (IDENTIFIER)
+ ] @function
+ (FnCallArguments)
+)
+
+(
+ [
+  variable_type_function: (IDENTIFIER)
+ ] @type
+ (FnCallArguments)
+ (#lua-match? @type "^[A-Z][a-z_0-9]*")
+)
+
+(
+ (ContainerField
+   (ErrorUnionExpr
+     (SuffixExpr
+       [
+        variable_type_function: (IDENTIFIER)
+        ] @constant
+       )
+     ))
+ )
 
 ;; @check if it's really necessary (too much color??)
 ; (
 ;  [
 ;   parameter: (IDENTIFIER)
 ;   variable_type_function: (IDENTIFIER)
-;  ] @type
-;  (#lua-match? @type "self")
+;  ] @constant.builtin
+;  (#lua-match? @constant.builtin "self")
 ; )
