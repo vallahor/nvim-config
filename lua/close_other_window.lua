bd8990b35dd8cb0fb7c2cc63c5b5d73355021426
Vallahor <vallahor91@gmail.com>
2023-10-22 22:09:46 -0300

    fix: close_other_window closing the current buffer when not moving to another (invalid movement)

--- lua/close_other_window.lua
+++ lua/close_other_window.lua
@@ -2,8 +2,9 @@ function CloseOtherWindow(direction)
 	local old_win = vim.api.nvim_get_current_win()
 	vim.cmd(string.format("wincmd %s", direction))
 	local win = vim.api.nvim_get_current_win()
+
 	vim.api.nvim_set_current_win(old_win)
-	if #vim.api.nvim_list_wins() > 1 then
+	if #vim.api.nvim_list_wins() > 1 and old_win ~= win then
 		vim.api.nvim_win_close(win, false)
 	end
 end
