f5efaa82afb5bede9d172cb4a42404aa9aec5c2d
Vallahor <vallahor91@gmail.com>
2025-07-26 18:12:46 -0300

    keymap - spider "W"'s working

--- lua/plugins/spider.lua
+++ lua/plugins/spider.lua
@@ -17,9 +17,30 @@ return {
       vim.keymap.set({ "n", "o", "x", "v" }, "w", "<cmd>lua require('spider').motion('w')<CR>")
       vim.keymap.set({ "n", "o", "x", "v" }, "e", "<cmd>lua require('spider').motion('e')<CR>")
       vim.keymap.set({ "n", "o", "x", "v" }, "b", "<cmd>lua require('spider').motion('b')<CR>")
-      vim.keymap.set({ "n", "o", "x", "v" }, "ge", "<cmd>lua require('spider').motion('ge')<CR>")
-      -- vim.keymap.set({ "n", "o", "x", "v" }, "q", "<cmd>lua require('spider').motion('ge')<CR>")
       vim.keymap.set({ "n", "o", "x", "v" }, "r", "<cmd>lua require('spider').motion('ge')<CR>")
+
+      vim.keymap.set(
+        { "n", "o", "x", "v" },
+        "W",
+        "<cmd>lua require('spider').motion('w', { subwordMovement = false })<CR>"
+      )
+      vim.keymap.set(
+        { "n", "o", "x", "v" },
+        "E",
+        "<cmd>lua require('spider').motion('e', { subwordMovement = false })<CR>"
+      )
+      vim.keymap.set(
+        { "n", "o", "x", "v" },
+        "B",
+        "<cmd>lua require('spider').motion('b', { subwordMovement = false })<CR>"
+      )
+      vim.keymap.set(
+        { "n", "o", "x", "v" },
+        "R",
+        "<cmd>lua require('spider').motion('ge', { subwordMovement = false })<CR>"
+      )
+      -- vim.keymap.set({ "n", "o", "x", "v" }, "ge", "<cmd>lua require('spider').motion('ge')<CR>")
+      -- vim.keymap.set({ "n", "o", "x", "v" }, "q", "<cmd>lua require('spider').motion('ge')<CR>")
 
       -- set "Q" to default "q"
       vim.keymap.set("n", "Q", "q")
