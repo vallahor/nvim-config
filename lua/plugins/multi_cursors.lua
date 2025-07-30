9efc33eaf4258b76154171ba88083cb0c55807b2
Vallahor <vallahor91@gmail.com>
2025-07-29 22:26:47 -0300

    multi cursors - sj - surround - paste insert

--- lua/plugins/multi_cursors.lua
+++ lua/plugins/multi_cursors.lua
@@ -1,35 +1,85 @@
 return {
   {
-    "mg979/vim-visual-multi",
-    event = "VeryLazy",
+    "jake-stewart/multicursor.nvim",
+    branch = "1.0",
     config = function()
-      vim.keymap.set({ "n", "v" }, "<a-a>", "<Plug>(VM-Reselect-Last)")
-      vim.keymap.set({ "n", "v" }, "<c-s-m>", "<Plug>(VM-Reselect-Last)")
-    end,
+      local mc = require("multicursor-nvim")
+      mc.setup()
 
-    -- init.lua FILE
-    -- -- VM --
-    -- vim.g.VM_theme = "iceblue"
-    -- vim.g.VM_default_mappings = 0
-    -- vim.g.VM_silent_exit = 1
-    -- vim.g.VM_custom_remaps = {
-    --   [")"] = "$",
-    --   ["("] = "0",
-    -- }
-    -- vim.g.VM_maps = {
-    --   ["Find Under"] = "<c-u>",
-    --   ["Find Subword Under"] = "<c-u>",
-    --   ["Select All"] = "<c-s-u>",
-    --   ["Add Cursor Down"] = "<c-j>",
-    --   ["Add Cursor Up"] = "<c-k>",
-    --   ["Switch Mode"] = "<tab>",
-    --   ["Align"] = "<c-a>",
-    --   ["Find Next"] = "]",
-    --   ["Find Prev"] = "[",
-    --   ["Goto Next"] = "}",
-    --   ["Goto Prev"] = "{",
-    --   ["Skip Region"] = "+",
-    --   ["Remove Region"] = "-",
-    -- }
+      local set = vim.keymap.set
+
+      -- Add a cursor for all matches of cursor word/selection in the document.
+      set({ "n", "x" }, "<c-u>", mc.matchAllAddCursors)
+      -- bring back cursors if you accidentally clear them
+      set("n", "Z", mc.restoreCursors)
+
+      -- Add or skip cursor above/below the main cursor.
+      set({ "n", "x" }, "<c-k>", function()
+        mc.lineAddCursor(-1)
+      end)
+      set({ "n", "x" }, "<c-j>", function()
+        mc.lineAddCursor(1)
+      end)
+      set({ "n", "x" }, "<c-s-k>", function()
+        mc.lineSkipCursor(-1)
+      end)
+      set({ "n", "x" }, "<c-s-j>", function()
+        mc.lineSkipCursor(1)
+      end)
+
+      -- Add or skip adding a new cursor by matching word/selection
+      set({ "n", "x" }, "<c-.>", function()
+        mc.matchAddCursor(1)
+      end)
+      set({ "n", "x" }, "<c->>", function()
+        mc.matchSkipCursor(1)
+      end)
+      set({ "n", "x" }, "<c-,>", function()
+        mc.matchAddCursor(-1)
+      end)
+      set({ "n", "x" }, "<c-<>", function()
+        mc.matchSkipCursor(-1)
+      end)
+
+      -- Add and remove cursors with control + left click.
+      set("n", "<c-leftmouse>", mc.handleMouse)
+      set("n", "<c-leftdrag>", mc.handleMouseDrag)
+      set("n", "<c-leftrelease>", mc.handleMouseRelease)
+
+      -- Append/insert for each line of visual selections.
+      -- Similar to block selection insertion.
+      set("v", "I", mc.insertVisual)
+      set("v", "A", mc.appendVisual)
+
+      -- Mappings defined in a keymap layer only apply when there are
+      -- multiple cursors. This lets you have overlapping mappings.
+      mc.addKeymapLayer(function(layerSet)
+        -- -- Select a different cursor as the main one.
+        -- layerSet({ "n", "x" }, "<left>", mc.prevCursor)
+        -- layerSet({ "n", "x" }, "<right>", mc.nextCursor)
+
+        -- -- Delete the main cursor.
+        -- layerSet({ "n", "x" }, "<leader>x", mc.deleteCursor)
+
+        -- Enable and clear cursors using escape.
+        layerSet("n", "<esc>", function()
+          if not mc.cursorsEnabled() then
+            mc.enableCursors()
+          else
+            mc.clearCursors()
+          end
+        end)
+      end)
+
+      -- Customize how cursors look.
+      local hl = vim.api.nvim_set_hl
+      hl(0, "MultiCursorCursor", { reverse = true })
+      hl(0, "MultiCursorVisual", { link = "Visual" })
+      hl(0, "MultiCursorSign", { link = "SignColumn" })
+      hl(0, "MultiCursorMatchPreview", { link = "Search" })
+      hl(0, "MultiCursorDisabledCursor", { reverse = true })
+      hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+      hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
+    end,
   },
 }
