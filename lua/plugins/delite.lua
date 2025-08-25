07c9637ed5d06ec881fb3dd12f6b26996835a54a
Vallahor <vallahor91@gmail.com>
2025-08-24 22:42:39 -0300

    comment in delite

--- lua/plugins/delite.lua
+++ lua/plugins/delite.lua
@@ -30,3 +30,25 @@ return {
     vim.keymap.set("n", "J", delite.join)
   end,
 }
+
+-- to check the behavior if needed
+-- M.next_word_normal_mode = function()
+-- 	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+-- 	local line = vim.api.nvim_get_current_line()
+-- 	local line_len = #line
+-- 	col = col + 1
+-- 	if col == line_len then
+-- 		local punctuation = line:sub(col, col):match("%p")
+-- 		if not punctuation then
+-- 			col = col + 1
+-- 		end
+-- 	end
+-- 	local new_pos = delete_word(row - 1, col, utils.direction.right)
+
+-- 	if new_pos then
+-- 		row, col = new_pos[1], new_pos[2]
+-- 		if col > 0 and col < #vim.api.nvim_get_current_line() then
+-- 			vim.api.nvim_win_set_cursor(0, { row + 1, col })
+-- 		end
+-- 	end
+-- end
