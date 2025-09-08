1512bae028e3588194884fb959b8168c7fc671fa
vallahor <vallahor91@gmail.com>
2025-09-08 13:24:21 -0300

    lsp(roslyn): back to using roslyn

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -146,7 +146,25 @@ return {
   },
   {
     "mason-org/mason.nvim",
-    opts = {},
+    opts = {
+      registries = {
+        "github:mason-org/mason-registry",
+        "github:Crashdummyy/mason-registry",
+      },
+      ensure_installed = {
+        "roslyn",
+      },
+    },
+    config = function(_, opts)
+      require("mason").setup(opts)
+      local mr = require("mason-registry")
+      for _, tool in ipairs(opts.ensure_installed) do
+        local p = mr.get_package(tool)
+        if not p:is_installed() then
+          p:install()
+        end
+      end
+    end,
   },
   {
     "mason-org/mason-lspconfig.nvim",
